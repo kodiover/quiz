acc8ae2e9d7144ee6f512dc85a15fec700cecedd
oned0t <itsoverkodi@gmail.com>
2021-07-24 15:41:30 +0100

    .

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,9 +1,11 @@
 
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+<script src="{{ mix('js/app.js') }}" defer></script>
+<body>
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+</body>
