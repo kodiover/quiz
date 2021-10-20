f80cde07be6ddd7ec021971ec655207899122f67
oned0t <66447952+oned0t@users.noreply.github.com>
2021-10-20 13:33:19 +0100

    Add commenting

--- database/migrations/2020_02_23_140150_create_questions_table.php
+++ database/migrations/2020_02_23_140150_create_questions_table.php
@@ -14,12 +14,12 @@ class CreateQuestionsTable extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->text('text', 400);
-            $table->unsignedBigInteger('quiz_id')->index();
-            $table->unsignedTinyInteger('time_limit');
-            $table->json('options');
-            $table->string('correct_key', 5);
+            $table->bigIncrements('id'); // Default Id incrementer
+            $table->text('text', 400); // The text key used to define the Qs text
+            $table->unsignedBigInteger('quiz_id')->index(); // Method used to add indicies to the column
+            $table->unsignedTinyInteger('time_limit'); // Set to positive only
+            $table->json('options'); // Method for adding extra data to a column
+            $table->string('correct_key', 5); // Method sets min value of 5
             $table->timestamps();
 
             $table->foreign('quiz_id')->references('id')->on('quizzes')->onDelete('cascade');
